489ea41a60e87212d2801dbf48e805da3f6ce3a5
Nelly Keppens <nelly.aporado@exocoder.io>
2025-05-28 13:55:02 +0200

    updated to scan range of ip's without scapy

--- scanner/run_scanner.py
+++ scanner/run_scanner.py
@@ -1,17 +1,23 @@
 # This is our entry file that a we use to run the application.
-from scanner.scanner_utils import parse_port_input
+from scanner.scanner_utils import parse_port_input, parse_ip_input
 from scanner.port_scanner import scan_ports
 
 def main():
     # target = input("Enter IP address to scan (default 127.0.0.1): ") or "127.0.0.1"
-    target = (input("Enter IP address to scan (default 127.0.0.1): ") or "127.0.0.1").strip() #added strip() to remove extra spaces
+    target_input = (input("Enter target IP or subnet (e.g. 192.168.56.0/28): ") or "127.0.0.1").strip()
     start_port = input("Enter start port (default 1): ")
     end_port = input("Enter end port (default 1024): ")
 
     try:
+        ip_list = parse_ip_input(target_input)
         start, end = parse_port_input(start_port, end_port)
-        open_ports = scan_ports(target, (start, end))
-        print(f"Open ports: {open_ports}")
+        # open_ports = scan_ports(target, (start, end))
+        # print(f"Open ports: {open_ports}")
+        for ip in ip_list:
+            print(f"\nScanning {ip}...")
+            open_ports = scan_ports(ip, (start, end))
+            print(f"Open ports on {ip}: {open_ports}")
+
     except ValueError as e:
         print(f"Error: {e}")
 
--- scanner/scanner_utils.py
+++ scanner/scanner_utils.py
@@ -1,3 +1,5 @@
+import ipaddress
+
 # This file is for scanning utilities: ICMP ping, SYN scan, parsing, etc.
 def validate_port_range(start, end):
     if 0 < start <= 65535 and 0 < end <= 65535 and start <= end:
@@ -9,3 +11,12 @@ def parse_port_input(start_str, end_str, default_start=1, default_end=1024):
     end = int(end_str) if end_str else default_end
     validate_port_range(start, end)
     return start, end
+
+
+def parse_ip_input(ip_input):
+    """Accepts a single IP or subnet (e.g., 192.168.56.0/28)"""
+    try:
+        network = ipaddress.ip_network(ip_input, strict=False)
+        return [str(ip) for ip in network.hosts()]
+    except ValueError as exc:
+        raise ValueError("Invalid IP address or subnet.") from exc
